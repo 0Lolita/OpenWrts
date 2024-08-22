8b2e3ae63ba6bff9857ab77e4bbf7e17d239b5f0
0Lolita <92529478+0Lolita@users.noreply.github.com>
2024-08-23 01:42:06 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -7,6 +7,13 @@ git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice
 git clone --depth 1 https://github.com/sirpdboy/luci-app-netwizard
 git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
 git clone --depth 1 https://github.com/sirpdboy/netspeedtest
+git clone --depth 1 https://github.com/sirpdboy/luci-app-advanced
+git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice
+git clone --depth 1 https://github.com/AlexZhuo/luci-app-bandwidthd
+git clone --depth 1 https://github.com/brvphoenix/luci-app-wrtbwmon
+git clone --depth 1 https://github.com/ilxp/luci-app-ikoolproxy
+git clone --depth 1 https://github.com/frainzy1477/luci-app-clash
+git clone --depth 1 https://github.com/OpenWrt-Actions/luci-app-vssr
 
 # Theme
 # luci-theme-neobird
